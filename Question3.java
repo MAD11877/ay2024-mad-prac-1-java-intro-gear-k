f2ec026a81e96b34ee53652608de3ce365356f4f
gear-k <150000892+gear-k@users.noreply.github.com>
2024-04-15 14:23:14 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
 
-public class Question3
-{
-  public static void main(String[] args)
-  {
-    /**
-     * Reads integer entered by the user, multiplies the integer by itself and print out the result.
-     * 
-     * Hint: in.nextInt() to read integer
-     */
-     
-    Scanner in = new Scanner(System.in);
-    
-  }
+public class Question3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        // Prompt user to enter an integer
+        System.out.print("Enter an integer: ");
+        int number = in.nextInt();
+
+        // Multiply the integer by itself
+        int squared = number * number;
+
+        // Print out the result
+        System.out.println("The square of " + number + " is " + squared);
+
+        // Close the scanner
+        in.close();
+    }
 }

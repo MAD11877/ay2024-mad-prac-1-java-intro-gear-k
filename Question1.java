35c038d3e5de45981f8d3b1e02336734e42e7ffe
gear-k <150000892+gear-k@users.noreply.github.com>
2024-04-15 14:15:41 +0800

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -1,11 +1,6 @@
-public class Question1
-{
-  public static void main(String[] args)
-  {
-    /**
-     * Print "Hello World!"
-     * 
-     * Hint: System.out.println(...)
-     */
-  }
+public class Question1 {
+    public static void main(String[] args) {
+        // Print "Hello World!"
+        System.out.println("Hello World!");
+    }
 }
